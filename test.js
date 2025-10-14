eedf0c70eb34efa83135fd783870f0eee8e96d43
hiba <hibazekraoui23@email.com>
2025-10-14 12:37:08 +0100

    hello

---
+++ test.js
@@ -0,0 +1,92 @@
+const prompt = require("prompt-sync")();
+/*let ex1 =  prompt("entrer votre informations personnelles");
+
+console.log("bonjour"+ ex1);*/
+
+
+
+/*
+let a=5;
+
+let b=7;
+
+let c = 2 ;
+
+a=b;
+
+b=c;
+
+c=a;
+
+console.log("a="+a,"b=" + b , "c=" + c);
+
+*/
+
+
+
+/*
+let a=parseInt(prompt("entrer le 1er nombre"));
+let b=parseInt(prompt("entrer le 2eme nombre"));
+
+let somme = a + b ;
+
+console.log(somme);
+*/
+
+/*
+let a=parseInt(prompt("entrer le 1er nombre"));
+let b=parseInt(prompt("entrer le 2eme nombre"));
+
+let sous= a - b ;
+
+console.log(sous);
+
+*/
+
+/*
+
+let a=parseInt(prompt("entrer le 1er nombre"));
+let b=parseInt(prompt("entrer le 2eme nombre"));
+
+let mul = a * b ;
+
+console.log(mul);
+
+*/
+ 
+/*
+
+let a=parseInt(prompt("entrer le 1er nombre"));
+let b=parseInt(prompt("entrer le 2eme nombre"));
+
+let div = a / b ;
+
+console.log(div);
+
+*/
+
+/*
+
+let a=parseInt(prompt("entrer le 1er nombre"));
+let b=parseInt(prompt("entrer le 2eme nombre"));
+
+let mod = a % b ;
+
+console.log(mod);
+
+*/
+
+/*
+
+let a = parseFloat(prompt("entrer le 1 er nombre"));
+let b = parseFloat(prompt("entrer le 2 eme nombre"));
+let c = parseFloat(prompt("entrer le 3 eme nombre"));
+let d = parseFloat(prompt("entrer le 4 eme nombre"));
+
+let somme = a + b + c + d ;
+
+let moyenne = somme / 4 ;
+
+console.log(moyenne);
+
+*/
