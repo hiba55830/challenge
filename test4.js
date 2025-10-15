d53d98b098c591fc38a095e449aabd0ef581c002
hiba <hibazekraoui23@email.com>
2025-10-15 17:37:12 +0100

    hello

---
+++ test4.js
@@ -0,0 +1,42 @@
+
+// 2
+/*
+let arr = [10,3,23,7,12,4,34,6,9,24];
+let somme = 0;
+
+for(let i = 0 ; i < arr.length; i++){
+  somme = somme + arr[i];
+}
+moyenne = somme / arr.length;
+console.log(somme);
+console.log(moyenne);
+*/
+
+//5 
+/*
+let arr = [23 , 14 , 3 , 20 , 31 , 9 , 10] ;
+
+let min = arr[0];
+let max = arr[0];
+
+for(let i=1 ; i < arr.length ; i++){
+   if(arr[i] > max){
+    max = arr[i];
+   }
+   if(arr[i] < min){
+    min = arr[i];
+   }
+}
+
+console.log(max + "est le plus grand");
+console.log(min + "est le plus petit");
+*/
+
+//6
+/*
+let arr = [23,7,65,92];
+for(let i = 0 ; i < arr.length; i++){
+    car = arr[i] * arr[i];
+}
+console.log(car);
+*/
